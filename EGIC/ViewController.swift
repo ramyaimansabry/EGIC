95ba3af98c80da5d312ff392bbf187327e7caa0e
ramyaymansabry <ramyaymansabry@ramys-macbook-pro.local>
2019-07-05 13:36:01 +0200

    Initial Commit

---
+++ EGIC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EGIC
+//
+//  Created by Ramy Ayman Sabry on 7/5/19.
+//  Copyright © 2019 Ramy Ayman Sabry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
